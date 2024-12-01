9e444fde9697330d156bdea698f48263ab073246
ajsalajsu <ajsalajsuz35@gmail.com>
2024-12-01 18:22:23 +0530

    d

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ async function send() {
 
 async function getAIResponse(userMessage) {
     try {
-        const response = await fetch('http://localhost:5505/api/chat', {
+        const response = await fetch('https://aichat-bot-2.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: userMessage })
